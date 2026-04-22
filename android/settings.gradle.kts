6287b69dabb925a6977aecf0e77b0a4bf14488a3
Kristóf Széles <47861477+kristofszeles@users.noreply.github.com>
2026-04-22 23:48:57 +0200

    chore: update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -6,5 +6,13 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "maze-game-android"
 include(":app")
